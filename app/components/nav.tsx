e4ca0b5edd829612d133f151464b8360f73e9b57
Lennart Reiher <lreiher@me.com>
2025-02-01 16:02:47 +0100

    draw borders around navbar items

--- app/components/nav.tsx
+++ app/components/nav.tsx
@@ -1,3 +1,5 @@
+'use client'
+import { usePathname } from 'next/navigation'
 import Link from 'next/link'
 
 const navItems = {
@@ -19,6 +21,8 @@ const navItems = {
 }
 
 export function Navbar() {
+  const currentPath = usePathname()
+  console.log('Current path:', currentPath)
   return (
     <aside className="-ml-[8px] mb-16 tracking-tight">
       <div className="lg:sticky lg:top-20">
@@ -28,11 +32,12 @@ export function Navbar() {
         >
           <div className="flex flex-row space-x-0 pr-10">
             {Object.entries(navItems).map(([path, { name }]) => {
+              const isActive = currentPath === path
               return (
                 <Link
                   key={path}
                   href={path}
-                  className="transition-all hover:text-neutral-800 dark:hover:text-neutral-200 flex align-middle relative py-1 px-2 m-1"
+                  className={`transition-all border border-transparent hover:border-neutral-800 hover:dark:border-neutral-200 rounded-lg flex align-middle relative py-1 px-2 m-1 ${isActive ? 'text-neutral-200 dark:text-neutral-800 bg-neutral-800 dark:bg-neutral-200 hover:text-neutral-200 hover:dark:text-neutral-800' : 'hover:text-neutral-800 hover:dark:text-neutral-200'}`}
                 >
                   {name}
                 </Link>
