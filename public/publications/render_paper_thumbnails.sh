f549ad85b10b652a607ad7b7f6a8efca1fedd045
Lennart Reiher <lreiher@me.com>
2025-10-04 13:57:36 +0200

    add application manager paper

--- public/publications/render_paper_thumbnails.sh
+++ public/publications/render_paper_thumbnails.sh
@@ -1,13 +1,13 @@
 #!/usr/bin/env bash
 
-# brew install imagemagick
+# brew install imagemagick gs
 
 if [ "$#" -ne 2 ]; then
     echo "Usage: $0 input_file output_file"
     exit 1
 fi
 
-convert "$1" -background white -alpha remove -alpha off "${1%.*}".p%02d.png
-convert +append "${1%.*}".p*.png "$2"
-convert "$2" -resize x512 "$2"
+magick convert "$1" -background white -alpha remove -alpha off "${1%.*}".p%02d.png
+magick convert +append "${1%.*}".p*.png "$2"
+magick convert "$2" -resize x512 "$2"
 rm "${1%.*}".p*.png
